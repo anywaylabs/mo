7e2297f976ed7e173917093fd4801e8c242f7508
Alexander Zinchuk <alx.zinchuk@gmail.com>
2017-03-01 19:25:21 +0300

    build_android logs

--- templates/app/phonegap/build_android.sh
+++ templates/app/phonegap/build_android.sh
@@ -1,3 +1,5 @@
+echo "Building for Android..."
 cp -rf ./res/* platforms/android/res
 phonegap build android --release -- --keystore="./secure/app.keystore" --storePassword=YOUR_PASSWORD_HERE --alias=app --password=YOUR_PASSWORD_HERE
 open platforms/android/build/outputs/apk/
+echo "Building for Android done."
