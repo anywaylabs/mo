a73b188dcb479847396f0103438992e17b69130b
Alexander Zinchuk <alx.zinchuk@gmail.com>
2017-03-03 17:01:31 +0300

    Small code style fix

--- templates/app/src/app/views/components/menu/Menu.js
+++ templates/app/src/app/views/components/menu/Menu.js
@@ -1,6 +1,6 @@
 import Panel from 'mo/views/components/Panel';
 import template from './Menu.hbs';
-import styles from './menu.less';
+import './menu.less';
 
 class Menu extends Panel {
     constructor() {
