4926a8f26d5eddb26a258a1735fcb2fdd3662307
Alexander Zinchuk <alx.zinchuk@gmail.com>
2018-03-12 01:34:32 +0100

    Inherit mobile app version from package.json

--- templates/app/phonegap/hooks/before_prepare/inc_build_version.sh
+++ templates/app/phonegap/hooks/before_prepare/inc_build_version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+
+main_version=$(node -p -e "require('../package.json').version")
+sed -E -i '' "s/version=\"[0-9\.]+\"/version=\"${main_version}\"/" config.xml
+echo "Package version set to ${main_version}"
 
 current=$(cat config.xml | grep -oE versionCode=\"\\d+\" | grep -oE \\d+)
 next=$((current+1))
-sed -E -i '' "s/android-versionCode=\"[0-9]+\"/android-versionCode=\"$next\"/" config.xml
-sed -E -i '' "s/ios-CFBundleVersion=\"[0-9]+\"/ios-CFBundleVersion=\"$next\"/" config.xml
-echo "Build version raised to $next from $current"
+sed -E -i '' "s/android-versionCode=\"[0-9]+\"/android-versionCode=\"${next}\"/" config.xml
+sed -E -i '' "s/ios-CFBundleVersion=\"[0-9]+\"/ios-CFBundleVersion=\"${next}\"/" config.xml
+echo "Build version raised to ${next} from ${current}"
